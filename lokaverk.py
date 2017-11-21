64d3f38a3837e61645432d7bca9fcd9f6ef90748
Qvorum <hakonha213@nemi.tskoli.is>
2017-11-21 11:57:43 +0000

    Transformers
    Breytir random tölunni í  sýnilega teninga

--- lokaverk.py
+++ lokaverk.py
@@ -2,8 +2,24 @@
 #Lokaverkefni í Forritun 2
 #21.11.2017
 import random
+def teningaUmbreytir(teningur):
+    if teningur == 1:
+        teningurVis = "⚀"
+    if teningur == 2:
+        teningurVis = "⚁"
+    if teningur == 3:
+        teningurVis = "⚂"
+    if teningur == 4:
+        teningurVis = "⚃"
+    if teningur == 5:
+        teningurVis = "⚄"
+    if teningur == 6:
+        teningurVis = "⚅"
+    return teningurVis
 teningur = random.randint(1,6)
 hamsturLocation = random.randint(1,75)
 rottaLocation1 = random.randint(1,100)
 rottaLocation2 = random.randint(1,100)
 rottaLocation3 = random.randint(1,100)
+musLocation    = random.randint(1,75)
+print("-------Nagdýraleikur--------")
