0b14f2113096820d40f4d1194be51ca2515c4c22
Qvorum <hakonha213@nemi.tskoli.is>
2017-11-21 11:36:31 +0000

    D.I.C.E
    Gerði Teninga kast og Byrjunarreiti

--- lokaverk.py
+++ lokaverk.py
@@ -1,3 +1,9 @@
 #Hákon Haraldsson & Viðar Marel
 #Lokaverkefni í Forritun 2
 #21.11.2017
+import random
+teningur = random.randint(1,6)
+hamsturLocation = random.randint(1,75)
+rottaLocation1 = random.randint(1,100)
+rottaLocation2 = random.randint(1,100)
+rottaLocation3 = random.randint(1,100)
